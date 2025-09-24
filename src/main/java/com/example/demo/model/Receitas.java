3c023f79657c0bb58b18ce8549d0737467f10ac5
sschettinii <andreschettini2006@gmail.com>
2025-09-24 19:36:14 -0300

    primeiro commit

---
+++ src/main/java/com/example/demo/model/Receitas.java
@@ -0,0 +1,68 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name ="receitas")
+public class Receitas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome não pode estar em branco")
+    @Size(max = 30, message = "O nome deve ter no máximo 30 caracteres")
+    private String nome;
+
+    @NotBlank(message = "Os ingredientes não pode estar em branco")
+    @Size(max = 250, message = "Os ingredientes deve ter no máximo 250 caracteres")
+    private String ingredientes;
+
+    @NotBlank(message = "O modo de preparo não pode estar em branco")
+    @Size(max = 1000, message = "O modo de preparo deve ter no máximo 1000 caracteres")
+    private String modoDePreparo;
+
+    @NotBlank("O tempo de preparo não pode estar em branco")
+    private LocalTime tempoDePreparo;
+
+    public Receitas() {}
+
+    public Receitas(String nome, String ingredientes, String modoDePreparo, LocalTime tempoDePreparo) {
+        this.nome = nome;
+        this.ingredientes = ingredientes;
+        this.modoDePreparo = modoDePreparo;
+        this.tempoDePreparo = tempoDePreparo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+    public void setIngredientes() { this.ingredientes = ingredientes}
+
+    public String getModoDePreparo() {
+        return modoDePreparo;
+    }
+    public void setModoDePreparo() { this.modoDePreparo = modoDePreparo}
+
+    public String getTempoDePreparo() {
+        return tempoDePreparo;
+    }
+    public void setTempoDePreparo() { this.tempoDePreparo = tempoDePreparo}
+}
